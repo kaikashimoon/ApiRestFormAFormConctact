451dfcde3fe773401aa4013cedbb6a1c87dff8d4
Carmen Leon <61640662+kaikrmen@users.noreply.github.com>
2024-05-30 13:30:16 -0400

    readme fixed and update code

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
-# ApiRestFormContact
-Using Nodemail and express 
+# Express Server with Nodemailer
 
-Just the Api configuration
+This project sets up an Express server that uses Nodemailer to send emails. It includes the use of environment variables and support for CORS.
+
+## Requirements
+
+- Node.js
+- npm (Node Package Manager)
+
+## Installation
+
+1. Clone this repository:
+
+    ```bash
+    git clone https://github.com/your-username/nodemailer-express.git
+    cd nodemailer-express
+    ```
+
+2. Install dependencies:
+
+    ```bash
+    npm install
+    ```
+
+3. Create a `.env` file in the root of the project and configure the following environment variables:
+
+    ```env
+    PORT=5000
+    MAIL_USERNAME=your_email@domain.com
+    MAIL_PASSWORD=your_password
+    MAIL_HOST=smtp.office365.com
+    MAIL_PORT=587
+    MAIL_USER=recipient@domain.com
+    ```
+
+## Usage
+
+To start the server, use the following command:
+
+```bash
+npm start
+```
+
+The server will listen on the port defined in the .env file or port 5000 by default.
+
+### Endpoint: POST /api/v1/contact
+
+**Description:**  
+Sends an email with the provided data.
+
+**URL:**  
+`/api/v1/contact`
+
+**Method:**  
+`POST`
+
+**Request Body:**
+
+```json
+{
+  "name": "Sender's name",
+  "email": "Sender's email",
+  "number": "Sender's phone number",
+  "text": "Sender's message"
+}
+```
+
+**Success Response:**
+
+```json
+{
+  "message": "Message sent successfully!"
+}
+```
+
+**dotenv** is used to manage environment variables.
+**express** is configured to handle JSON and URL-encoded requests.
+**cors** is configured to allow requests from any origin.
+**nodeMail** is used to send emails with the contact form details.
+The **/v1/contact** endpoint receives the form data and uses mainMail to send the email.
--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 require("dotenv").config();
 const express = require("express");
 const nodeMail = require("nodemailer");
-const path = require("path");
 const cors = require("cors");
-const { resolve } = require("path");
-
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
-
-app.use(cors({
-    origin: '*',
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-  }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
 
 async function mainMail(name, email, number, text) {
-  const transporter = await nodeMail.createTransport({
-    host: "mail.kamaleont.io",
-    port: 465,
-    secure: true, 
-      auth: {
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD,
-      }, tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false,
-      },
+  const transporter = nodeMail.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: false, // true in production
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
   });
   const mailOption = {
     from: process.env.MAIL_USERNAME,
-    to: process.env.MAIL_USER,
-    subject: "Nuevo mensaje desde la web kamaleont",
+    to: process.env.MAIL_USER || email, // Use a specif email or the user email or dynamic email 
+    subject: "New Email",
     html: `<b> You got a message from  <br />
     Email : ${email} <br />
     Name: ${name} <br />
@@ -46,23 +43,22 @@ async function mainMail(name, email, number, text) {
     await transporter.sendMail(mailOption);
     return Promise.resolve("Message Sent Successfully!");
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return Promise.reject(error);
   }
 }
 
-app.post("/v1/contact", async (req, res, next) => {
-  const { name, email, number, text} = req.body;
+app.post("/api/v1/contact", async (req, res, next) => {
+  const { name, email, number, text } = req.body;
   try {
     const data = await mainMail(name, email, number, text);
-    console.log(data)
-    res.status(200).json({message: "Message send successfully!"});
+    console.log(data);
+    res.status(200).json({ message: "Message send successfully!" });
   } catch (error) {
-    return res.status(500).json({ message: error });  }
+    return res.status(500).json({ message: error });
+  }
 });
-
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
-
